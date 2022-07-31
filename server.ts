98606da4e1d3ce1d81ffd3be79f0be0432e057ad
Neli Harbuzava <ineliharbuzava@gmail.com>
2022-07-31 19:05:45 +0300

    Test web setup

---
+++ server.ts
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello World123!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
